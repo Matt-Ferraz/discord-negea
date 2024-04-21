401a68511e14ca8deda7f723201870c68425d149
Mateus Ferraz <108159617+Matt-Ferraz@users.noreply.github.com>
2024-04-21 14:31:58 -0300

    add new commands and new message to missing arguments

--- utils/eventos.py
+++ utils/eventos.py
@@ -16,19 +16,30 @@ async def message_handler(message: str, client):
 
     if "champ" in message.content.lower():
         if len(message.content.split()) < 2:
-            return await message.channel.send("Invalid command, missing champion name. \nUsage: #champ [name]")
+            return await message.channel.send("Invalid command, too few arguments. \nUsage: #champ [name]")
 
-        champ_name = message.content.split()[1]   
+        champ_name = message.content.split()[1]
 
         if "list" in message.content.lower():
             return await message.channel.send("In development")
 
-     
         champ_text = await get_champ_by_name(champ_name.capitalize())
-        
+        champ_data = parse_response(champ_text)['data'][champ_name.capitalize()]
+
         if "Access Denied" in champ_text:
             return await message.channel.send("Champion {} not found. Get the full list by #champs list".format(champ_name.capitalize()))
 
-        champ_data = parse_response(champ_text)['data'][champ_name.capitalize()]
+        if len(message.content.split()) >= 3:
+            arg = message.content.split()[2]
+
+            if arg == 'tips':
+                allytips = "".join(champ_data['allytips'])
+
+                return await message.channel.send(allytips)
+            
+            if arg == 'passive':
+                passive = 'Nome da passiva: %s \nDescri'.format(champ_data['passive']['name'])
+
+
         title = champ_data['title']
         await message.channel.send(title)
