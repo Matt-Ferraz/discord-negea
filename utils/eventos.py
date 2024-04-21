77e5a49ec6a7337a44a28229fd71626b80f274e5
Mateus Ferraz <108159617+Matt-Ferraz@users.noreply.github.com>
2024-04-21 12:25:00 -0300

    add command to search for champ's title

--- utils/eventos.py
+++ utils/eventos.py
@@ -1,4 +1,8 @@
-from utils.riot import test_url
+from utils.riot import test_url, get_champ_by_name
+import json
+
+def parse_response(str: str) -> json:
+    return json.loads(str)
 
 async def message_handler(message: str, client):
     if "riot" in message.content.lower():
@@ -8,3 +12,10 @@ async def message_handler(message: str, client):
 
     if "bibi" in message.content.lower():
         await message.channel.send(':blue_car: :blue_car: :blue_car: :blue_car:')
+    
+    if "champ" in message.content.lower():
+        if len(message.content.split()) > 1 and message.content.split()[1] is not None:
+            champ_text = await get_champ_by_name(message.content.split()[1].capitalize())
+            champ_data = parse_response(champ_text)['data'][message.content.split()[1].capitalize()]
+            title = champ_data['title']
+            await message.channel.send(title)
--- utils/riot.py
+++ utils/riot.py
@@ -1,12 +1,20 @@
 import os
 from utils.https_util import get_request
 from dotenv import load_dotenv
+
 load_dotenv()
 
 RIOT_URL_LOL = os.getenv('RIOT_URL_LOL')
 RIOT_KEY_DEVELOPMENT = os.getenv('RIOT_KEY_DEVELOPMENT')
-
 rotation_route = "platform/v3/champion-rotations"
+CHAMPS_URL = "https://ddragon.leagueoflegends.com/cdn/14.8.1/data/en_US/champion.json"
+CHAMP_URL = "https://ddragon.leagueoflegends.com/cdn/14.8.1/data/en_US/champion/"
+
+async def get_champ_by_name(name):
+    return await get_request(CHAMP_URL + name + ".json")
 
 async def test_url():
+    return await get_request(RIOT_URL_LOL + rotation_route + "?api_key=" + RIOT_KEY_DEVELOPMENT)
+
+async def get_all_champs():
     return await get_request(RIOT_URL_LOL + rotation_route + "?api_key=" + RIOT_KEY_DEVELOPMENT)
