f65f062539ea28c2b3e66272413c696029455803
Mateus Henrique Ferraz <ferraz_dev@proton.me>
2024-04-22 14:27:59 -0300

    add embed message with champ icon, user icon and nickname, add more information to champ structure

--- utils/eventos.py
+++ utils/eventos.py
@@ -1,10 +1,25 @@
 from utils.riot import test_url, get_champ_by_name
+from utils.embed_formatters import format_embed_champion
 import json
 
-def parse_response(str: str) -> json:
+def format_champ_data(raw, name):
+    print(raw)
+    formatted = {
+        'passive_name': raw['passive']['name'],
+        'passive_desc': raw['passive']['description'],
+        'title': raw['title'],
+        'name': name,
+        'image_url': raw['image']['full']
+    }
+    return formatted
+
+
+def parse_response(str: str):
     return json.loads(str)
 
-async def message_handler(message: str, client):
+async def message_handler(message, client):
+    print(message.author.avatar)
+
     if "riot" in message.content.lower():
         champs = await test_url()
         await message.channel.send("``` \n" + champs +"\n ```")
@@ -12,25 +27,25 @@ async def message_handler(message: str, client):
 
     if "bibi" in message.content.lower():
         await message.channel.send(':blue_car: :blue_car: :blue_car: :blue_car:')
-    
+
 
     if "champ" in message.content.lower():
         if len(message.content.split()) < 2:
             return await message.channel.send("Invalid command, too few arguments. \nUsage: #champ [name]")
 
-        champ_name = message.content.split()[1]
+        champ_name = message.content.split()[1].capitalize()
 
         if "list" in message.content.lower():
             return await message.channel.send("In development")
-        
-        champ_text = await get_champ_by_name(champ_name.capitalize())
-        
-        if "Access Denied" in champ_text: 
-            return await message.channel.send("Champion {} not found. Get the full list by #champs list".format(champ_name.capitalize()))
 
-        champ_data = parse_response(champ_text)['data'][champ_name.capitalize()]
+        champ_text = await get_champ_by_name(champ_name)
 
-      
+        if "Access Denied" in champ_text:
+            return await message.channel.send("Champion {} not found. Get the full list by #champs list".format(champ_name))
+
+        champ_parsed = parse_response(champ_text)['data'][champ_name]
+        champ_data = format_champ_data(champ_parsed, champ_name)
+
         if len(message.content.split()) >= 3:
             arg = message.content.split()[2]
 
@@ -38,11 +53,11 @@ async def message_handler(message: str, client):
                 allytips = "".join(champ_data['allytips'])
 
                 return await message.channel.send(allytips)
-            
+
             if arg == 'passive':
                 print(champ_data['passive']['name'], champ_data['passive']['description'])
                 passive = "Nome da passiva: " + champ_data['passive']['name'] + "\nDesc: " + champ_data['passive']['description']
                 return await message.channel.send(passive)
 
-        title = champ_data['title']
-        await message.channel.send(title)
+        embed_champ = format_embed_champion(champ_data, message.author)
+        await message.channel.send(embed=embed_champ)
