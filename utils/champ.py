fe70a871f2048da81b9faa9a3c3d585e6290a17e
Mateus Henrique Ferraz <ferraz_dev@proton.me>
2024-04-23 10:39:26 -0300

    fix: spells keybinds and HTML parser

--- utils/champ.py
+++ utils/champ.py
@@ -1,12 +1,58 @@
+from io import StringIO
+from html.parser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.text = StringIO()
+    def handle_data(self, d):
+        self.text.write(d)
+    def get_data(self):
+        return self.text.getvalue()
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+list_spells = ["Q", "W", "E", "R"]
+
 def format_spells(spells, name):
     formated_dict = {}
+    count = 0
     for spell in spells:
-        key_by_spell_keybind = spell['id'].replace(name, '').upper()
-        formated_dict[key_by_spell_keybind] = {
-            'name': spell['name'],
-            'desc': spell['description'],
-            'image_url': "https://ddragon.leagueoflegends.com/cdn/12.4.1/img/spell/" +spell['image']['full']
-        }
+            spell_name = spell["name"]
+            spell_description = strip_tags(spell["description"])
+            spell_tooltip = spell["tooltip"]
+            spell_max_rank = spell["maxrank"]
+            spell_cooldowns = spell["cooldown"]
+            spell_costs = spell["cost"]
+            spell_range = spell["range"]
+
+            formated_dict[list_spells[count]] = {
+                "description": spell_description,
+                "tooltip": spell_tooltip,
+                "max_rank": spell_max_rank,
+                "cooldowns": spell_cooldowns,
+                "costs": spell_costs,
+                "range": spell_range
+            }
+            count+=1
+
+    # Printing spell information
+    for spell_name, spell_info in formated_dict.items():
+        print(f"Spell Name: {spell_name}")
+        print(f"Description: {spell_info['description']}")
+        print(f"Tooltip: {spell_info['tooltip']}")
+        print(f"Max Rank: {spell_info['max_rank']}")
+        print(f"Cooldowns: {spell_info['cooldowns']}")
+        print(f"Costs: {spell_info['costs']}")
+        print(f"Range: {spell_info['range']}")
+        print("\n")
 
     return formated_dict
 
